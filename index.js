da3b26c4e8ad9c9e21ec6808f706496f9ae5a46a
ayat273 <ayatgamal67@gmail.com>
2024-09-29 21:44:14 +0300

    Upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let input = document.getElementById("box");
+let list = document.getElementById("list");
+function addTask() {
+  if (input.value === "") {
+    alert("You Must Writ Something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+  saveData();
+}
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
+}
+function showData() {
+  list.innerHTML = localStorage.getItem("data");
+}
+showData();
